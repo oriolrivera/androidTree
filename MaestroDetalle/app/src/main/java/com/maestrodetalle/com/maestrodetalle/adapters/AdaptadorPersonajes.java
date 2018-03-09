480b9d440789b4c5c586c33dac21ba6ce7fdde90
sheolseol <siolsiol@hotmail.com>
2018-03-08 22:31:11 -0400

    Adapter personajes

---
+++ MaestroDetalle/app/src/main/java/com/maestrodetalle/com/maestrodetalle/adapters/AdaptadorPersonajes.java
@@ -0,0 +1,67 @@
+package com.maestrodetalle.com.maestrodetalle.adapters;
+
+/**
+ * Created by USER on 3/8/2018.
+ */
+
+public class AdaptadorPersonajes extends RecyclerView.Adapter<AdaptadorPersonajes.PersonajesViewHolder> implements View.OnClickListener  {
+    ArrayList<PersonajeVo> listaPersonajes;
+    private View.OnClickListener listener;
+
+    public AdaptadorPersonajes(ArrayList<PersonajeVo> listaPersonajes) {
+        this.listaPersonajes = listaPersonajes;
+    }
+
+    @Override
+    public PersonajesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list,null,false);
+        RecyclerView.LayoutParams layParams = new RecyclerView.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        view.setLayoutParams(layParams);
+
+        view.setOnClickListener(this);
+
+        return new PersonajesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(PersonajesViewHolder holder, int position) {
+        holder.txtNombre.setText(listaPersonajes.get(position).getNombre());
+        if (Utilidades.PORTRAIT==true){
+            holder.txtInformacion.setText(listaPersonajes.get(position).getInfo());
+        }
+
+        holder.foto.setImageResource(listaPersonajes.get(position).getImagenId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaPersonajes.size();
+    }
+
+    public void setOnClickListener(View.OnClickListener listener){
+        this.listener=listener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (listener!=null){
+            listener.onClick(view);
+        }
+    }
+
+    public class PersonajesViewHolder extends RecyclerView.ViewHolder {
+        TextView txtNombre,txtInformacion;
+        ImageView foto;
+
+        public PersonajesViewHolder(View itemView) {
+            super(itemView);
+            txtNombre= (TextView) itemView.findViewById(R.id.idNombre);
+            if (Utilidades.PORTRAIT==true){
+                txtInformacion= (TextView) itemView.findViewById(R.id.idInfo);
+            }
+
+            foto= (ImageView) itemView.findViewById(R.id.idImagen);
+        }
+    }
+}
